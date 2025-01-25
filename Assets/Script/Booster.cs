83da220a9db056effe291f72ce83a291a0bc9dd9
Figgorine <viggo.asgarden@gmail.com>
2025-01-25 14:12:40 +0100

    Added prototype materials, updated viggo scene

---
+++ Assets/Script/Booster.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Booster : MonoBehaviour
+{
+    public float boostValue = 10f;
+
+    public Vector3 targetDir;
+
+    private void Start() {
+        targetDir = transform.forward;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.GetComponent<Rigidbody>() != null)
+        {
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+
+            rb.AddForce(targetDir * boostValue, ForceMode.Impulse);
+            Debug.Log($"Add force to {other.gameObject.name} with a value of {boostValue} in the direction of {targetDir}");
+        }
+    }
+}
